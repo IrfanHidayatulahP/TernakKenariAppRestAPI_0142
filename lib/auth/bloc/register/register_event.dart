c2d6a7807b1e92aea9c11d22f77efacd9c493eb4
IrfanHidayatulahP <irfanhputra400@gmail.com>
2025-06-06 17:44:56 +0700

    menyesuaikan struktur dalam project

--- lib/auth/bloc/register/register_event.dart
+++
@@ -1,9 +0,0 @@
-part of 'register_bloc.dart';
-
-sealed class RegisterEvent {}
-
-class RegisterRequested extends RegisterEvent {
-  final RegisterRequestModel requestModel;
-
-  RegisterRequested({required this.requestModel});
-}
